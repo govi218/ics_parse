d68ced6cd2be0c32dc7ed4f67e5c65e352e5d20b
Govind Mohan <govi218mu@gmail.com>
2017-10-08 19:52:38 -0400

    first commit

---
+++ parse_cal.py
@@ -0,0 +1,32 @@
+from icalendar import Calendar, Event
+from datetime import datetime
+import time
+
+
+cal_ics = open('example.ics')
+gcal = Calendar.from_ical(cal_ics.read())
+employee_dict = {}
+for component in gcal.walk():
+	if component.name == "VEVENT":
+		print component.get('summary')
+		start = component.get('dtstart')
+		end = component.get('dtend')
+		print start.dt
+		print end.dt
+		out = "duration: " + str(end.dt - start.dt)
+		print out
+		wage = str(15.75*((int(time.mktime(end.dt.timetuple())) 
+			- int(time.mktime(start.dt.timetuple())))/3600))
+		print "wage: " + wage + '\n'
+
+		if employee_dict.has_key(component.get('summary')):
+                        employee_dict[component.get('summary')] += wage
+                else:
+                        employee_dict[component.get('summary')] = wage
+
+
+print '\n'
+
+for employee in employee_dict:
+        print employee + " " + str(employee_dict[emp]) + '\n'
+cal_ics.close()
